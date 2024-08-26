54d81f085b6204fba60ab3daa7223369c4847799
Gael <108774720+Gaeljs@users.noreply.github.com>
2024-08-26 16:52:29 +0200

    C2: extraction des constantes

---
+++ const.js
@@ -0,0 +1,15 @@
+// const.js
+
+const CONFIG = {
+    title: 'Hello world',
+    labelText: 'Hello world',
+    labelWidth: '200px',
+    labelHeight: '200px',
+    labelTextColor: 'center',
+    labelBackgroundColor: 'yellow',
+    labelTextAlign: 'center',
+    labelDisplay: 'flex',
+    labelAlignItems: 'center',
+    labelJustifyContent: 'center',
+    labelMargin: '0 auto'
+};
--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("ok");
+// script.js
+
+function helloWorld() {
+    // Appliquer le titre de la fenêtre
+    document.title = CONFIG.title;
+
+    // Création du label de texte
+    const label = document.createElement('div');
+    label.textContent = CONFIG.labelText;
+    label.style.textAlign = CONFIG.labelTextAlign;
+
+    // Appliquer les styles au label
+    label.style.backgroundColor = CONFIG.labelBackgroundColor;
+    label.style.width = CONFIG.labelWidth;
+    label.style.height = CONFIG.labelHeight;
+    label.style.display = CONFIG.labelDisplay;
+    label.style.alignItems = CONFIG.labelAlignItems;
+    label.style.justifyContent = CONFIG.labelJustifyContent;
+    label.style.margin = CONFIG.labelMargin;
+
+    // Ajouter le label à la fenêtre (au corps du document)
+    document.body.appendChild(label);
+}
+
+// Appeler la fonction pour exécuter l'algorithme
+helloWorld();
